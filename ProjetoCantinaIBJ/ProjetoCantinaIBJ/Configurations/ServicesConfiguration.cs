37f5816f2b486b634880702cc65b73f7dc63995a
felipe33081 <79608511+felipe33081@users.noreply.github.com>
2023-03-28 23:18:56 -0300

    Add paste common, configurations and Core in controllers

---
+++ ProjetoCantinaIBJ/ProjetoCantinaIBJ/Configurations/ServicesConfiguration.cs
@@ -0,0 +1,10 @@
+﻿namespace CantinaIBJ.WebApi.Configurations;
+
+public static class ServicesConfiguration
+{
+    public static IServiceCollection ConfigureRepositories(this IServiceCollection services)
+    {
+
+        return services;
+    }
+}
