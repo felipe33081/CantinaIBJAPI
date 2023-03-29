0af33f85e8ea8f3a8092411648094b5cfdc65e1d
felipe33081 <79608511+felipe33081@users.noreply.github.com>
2023-03-28 23:22:59 -0300

    change program

--- ProjetoCantinaIBJ/ProjetoCantinaIBJ/Program.cs
+++ ProjetoCantinaIBJ/ProjetoCantinaIBJ/Program.cs
@@ -1,25 +1,72 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllersWithViews();
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//builder.Services.AddEntityFrameworkNpgsql()
+//    .AddDbContext<PostgreSqlContext>(options => options.UseNpgsql(
+//        builder.Configuration.GetConnectionString("POSTGRESQLCONNSTR_PostgreSQL")
+//        ));
+
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Cadastro de Produtos", Version = "v1" });
+});
+
+builder.Services.AddMvcCore().AddAuthorization().AddDataAnnotations();
+builder.Services.AddMemoryCache();
+builder.Services.AddCors(c =>
+{
+    c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
+builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+//builder.Services.AddScoped<MapperProfile>();
+//builder.Services.AddScoped<IProductRepository, ProductRepository>();
+//builder.Services.AddScoped<IProviderRepository, ProviderRepository>();
 
 var app = builder.Build();
 
+app.UseSwaggerUI();
+app.UseSwagger(x => x.SerializeAsV2 = true);
+
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (!app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllers();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
+app.UseCors(options =>
+{
+    options.WithOrigins("http://localhost:3000")
+    .AllowAnyMethod()
+    .AllowAnyHeader();
+});
+
+app.UseDeveloperExceptionPage();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
